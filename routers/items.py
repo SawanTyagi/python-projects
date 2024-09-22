8a358a720de5a928144fc36c8d1b6adec2687bfe
Sawan Tyagi <sawan2207@gmail.com>
2024-09-22 20:54:05 +0530

    learning till Query Parameter Models done

--- routers/items.py
+++ routers/items.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, Path
+
+from typing import Annotated, Literal
+
+from pydantic import BaseModel, Field
 
 from ..model_classes.item import Item
 
@@ -30,8 +34,28 @@ async def create_item(item: Item):
 
 
 @router.put('/items/{item_id}')
-async def update_item(item_id: int, item: Item, q: str | None = None):
+async def update_item(item_id: Annotated[int, Path(title="The ID of item to get")],
+                      item: Item,
+                      q: Annotated[str | None, Query(max_length=50, min_length=3)] = None):
     result = {"item_id": item_id, **item.model_dump()}
     if q:
         result.update({"q": q})
     return result
+
+
+@router.get('/items_with_query_paramerter_multiple_values/')
+async def items_with_query_paramerter_multiple_values(
+        q: Annotated[list[str] | None, Query(max_length=50, min_length=3)] = None):
+    return {"q": q}
+
+
+class FilterModel(BaseModel):
+    limit: int = Field(100, gt=0, le=100)
+    offset: int = Field(0, ge=0)
+    order_by: Literal["created_at", "updated_at"] = "created_at"
+    tags: list[str] = []
+
+
+@router.get("/getQueryModelParams")
+async def get_query_model_param(query_filter: Annotated[FilterModel, Query()]):
+    return query_filter
