a345859139a9aa88e47d94b9b9f8893ffa705463
Sawan Tyagi <sawan2207@gmail.com>
2024-09-25 20:50:48 +0530

    learning till Body Nested Models done

--- model_classes/item.py
+++ model_classes/item.py
@@ -1,8 +1,15 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, HttpUrl
+
+
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
 
 
 class Item(BaseModel):
     name: str
     description: str | None = Field(default=None, title="The description of the item", max_length=200)
     price: float = Field(ge=0, description="The price must be greater than zero", )
-    tax: float | None = None
+    tax: float | None = None,
+    tags: set[str] = set(),
+    image: Image | None = None
